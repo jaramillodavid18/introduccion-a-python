1779ea0e96894d832290007279faf9869e95d3f3
jaramillodavid18 <jaramillo.david@correounivalle.edu.co>
2025-08-29 10:01:37 -0500

    ejercicio1_4

---
+++ operadores.py
@@ -0,0 +1,66 @@
+'''
+operadores matematicos 
+suma
+resta
+división 
+multiplicación 
+potencia
+floor division
+modulo
+'''
+
+x= 2
+y= 5
+#print(x+y)
+
+w= 2.5
+#print(type(x), type(y))
+z= x +w
+#print(z, type(z))
+#operador resta
+a=10
+b=15
+c=b-a
+#print("el resultado de la resta es:",c, type (c))
+
+#operador división
+d=12
+f=4
+#print(d/f, type(d/f))
+#operador multiplicación
+a=50
+print(2*a)
+#potencia
+y=a**f
+print(f"el numero {a} elevado a la variable f {f} es {y}")
+# división piso
+m=50
+n=3
+#print(m//n)
+
+#modulo (%)
+g=10
+h=3
+#print(g%h) # respuesta es el residuo 
+
+y= 8//-3 
+#print(y)
+y=8/-3
+#print(y)
+'''
+Este tipo de operadores los usamos cuando deseamos comparar expresiones o
+variables. Python evalúa si se cumple la comparación y nos devuelve (retorna) un
+valor True o False
+'''
+
+#es igual a ==
+print(2==2)
+print(2==2.0)
+
+#es diferente !=
+print("univalle" !="Univalle")
+
+x= 4.123456789000001058946546
+y= 4.123456789000001
+
+print("la variable x es igual a la variable y?",x==y)
